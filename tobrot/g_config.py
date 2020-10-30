08baeef2c4ec71ffaf1249cd6cda646c7c70c0e8
manimaran-1 <68263470+manimaran-1@users.noreply.github.com>
2020-10-30 20:44:24 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -2,11 +2,12 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-    TG_BOT_TOKEN= "1268fill---your----data"
-    APP_ID = 1373938
-    API_HASH = "fill--your--data"
-    OWNER_ID = 12537936
-    AUTH_CHANNEL = [-10082786282972]
-    DESTINATION_FOLDER = "HelloMan" #Name of your folder read readme
-    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
-    #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
+	TG_BOT_TOKEN= "1379911592:AAFkjpVY5PaOvyj6TtWHEDXTvjRxWLmQAdw"
+	APP_ID = 1674832
+	API_HASH = "ae3433b211f7b86c3ee038997e4e1ad0"
+	OWNER_ID = "786418598" #ID of bot owner
+	AUTH_CHANNEL = [-1001302967369]
+	DESTINATION_FOLDER = "0AH_ZEIqAXEc7Uk9PVA" #Name of your folder read readme
+	RCLONE_CONFIG = """type = drive\nscope = drive\nroot_folder_id = 1zRhfAbKOWNrg2qh-VQcquRPGvWierrDG\ntoken = {"access_token":"ya29.a0AfH6SMCFFXK-Oex6lWBkx97c062enChMLpDOQ1fc8imqIhlBbDxgr4sGQO0fcXQMEQPn6iDaJVfEm1ezLb811j6ymm6VPm4gJPW-ledXEjJHbFh6Acmg2q7Jw3kMob6qC3SWiPQFnyy_ZiEv7xBQxntbVkpvKIpgp0JOiQ","token_type":"Bearer","refresh_token":"1//0gZXP7EYlLy7bCgYIARAAGBASNwF-L9IrlM2o0SdqKEMRPDw7UmuZMnK8cXqVQ3CP0TQxU0JyQrK0pMG741XD44tKDSr_DeU5exg","expiry":"2020-10-06T08:23:10.6304723+05:30"}"""
+	INDEX_LINK = "https://server3-washingtondc.mohanraj.workers.dev/0:/0AH_ZEIqAXEc7Uk9PVA/0AH_ZEIqAXEc7Uk9PVA"
+	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
